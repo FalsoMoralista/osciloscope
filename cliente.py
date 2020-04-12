d3f19fbcbdfb0b66f806297a0b776e458f4c6846
luciano <lucianoadfilho@gmail.com>
2020-04-12 00:53:08 -0300

    converted to udp

--- cliente.py
+++ cliente.py
@@ -3,53 +3,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from itertools import count
-#####CONNECTION#####
+
 host = '127.0.0.1'
 port = 7000
 
 addr = (host, port) 
 
-serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+socket.bind(addr) 
 
-serv_socket.bind(addr) 
-
-serv_socket.listen(10) 
-
-print ('aguardando conexao')
-
-con, cliente = serv_socket.accept()
-
-print ('conectado') 
-print ("aguardando mensagem")
-#####END CONNECTION############
-
-########GRAPH##################
-
-plt.style.use('fivethirtyeight')
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
+print('Listening ...')
 
 x = [0]
 xs = []
 ys = []
+
+plt.style.use('fivethirtyeight')
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
 def animate(i):
-    ystr = con.recv(1).decode()
-    y = int(ystr)
+    ystr, address = socket.recvfrom(1)
     xs.append(x[0])
-    ys.append(y)
+    ys.append(ystr)
     plt.cla()
     plt.plot(xs, ys, label='Channel 1')
-    plt.grid(True)
-    ax.set_title('Osciloscope')
-    ax.set_ylabel('Voltage')
-    ax.set_xlabel('Time (ms)')
-    ax.set_ylim(-5,6)
+    if x[0] % 10 == 0:
+        ys.clear()
+        xs.clear()
+#    plt.grid(True)
+#    ax.set_title('Osciloscope')
+#    ax.set_ylabel('Voltage')
+#    ax.set_xlabel('Time (ms)')
+#    ax.set_ylim(-5,6)
     x[0] += 1
-    plt.legend(loc='upper left')
-    
+#    plt.legend(loc='upper left')
 ani = FuncAnimation(plt.gcf(), animate, interval=0)
 plt.tight_layout()
 plt.show()
